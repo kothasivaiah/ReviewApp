eb38d1ab40e7d78f1f6e89f9ba6d1af266f9332e
kothasivaiah <kothasivaiah99@gmail.com>
2022-04-22 17:01:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vukc44
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vukc44)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+import Review from './Review';
+
+export default function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Review</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
